4c30c125a0c09e13ed234fd3b97418d4ded77570
levente2001 <kalolevente@gmail.com>
2023-07-28 19:36:57 +0200

    changes

---
+++ build/precache-manifest.28c7a1e416d064469e2be00afe6dbc0b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8154b20ab5c75874bb82532613d8c947",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bc039b5229f2c02912e",
+    "url": "/static/css/main.fe124fd2.chunk.css"
+  },
+  {
+    "revision": "ea0d48e79db3960a6d3d",
+    "url": "/static/js/2.d8067193.chunk.js"
+  },
+  {
+    "revision": "f9416c2fa5dace694260608407c8cf2e",
+    "url": "/static/js/2.d8067193.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bc039b5229f2c02912e",
+    "url": "/static/js/main.31c0a6ac.chunk.js"
+  },
+  {
+    "revision": "a1752a22879d9724e22f",
+    "url": "/static/js/runtime-main.f2a2eb5f.js"
+  },
+  {
+    "revision": "b6105883acf0edffd0c0b5bfb7a46d84",
+    "url": "/static/media/plane.b6105883.png"
+  }
+]);
